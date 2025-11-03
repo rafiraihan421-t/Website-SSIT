7f3493d0674f88d345b575952497bfe858f98e79
rafiraihan421-t <rafiraihan421@gmail.com>
2025-11-03 14:38:33 +0700

    Add files via upload

--- Website-SSIT/javascript/script.js
+++ Website-SSIT/javascript/script.js
@@ -1,22 +1,22 @@
+import { dataPengguna} from "./data.js";
 const test='Hit'
 console.log(test)
  const getInputEmail = document.getElementById('email');
   const getInputPassword = document.getElementById('password');
   const form = document.getElementById('loginForm');
-
-  // Regex sederhana
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const passwordRegex = /^[A-Za-z0-9!@#\$%\^&\*]{6,}$/; // minimal 6 karakter
   form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const email = getInputEmail.value.trim();
-    const password = getInputPassword.value.trim();
-    if (!emailRegex.test(email)) {
-      alert('Email tidak valid! Contoh: user@example.com');
+    e.preventDefault()
+      const getDataPengguna=dataPengguna.find((val)=>val.email===getInputEmail.value.trim())
+;
+    if (!getDataPengguna) {
+      console.log(getInputEmail.value)
+      console.log(dataPengguna)
+      console.log(getDataPengguna)
+      alert('Email tidak ditemukan');
       return;
     }
-    if (!passwordRegex.test(password)) {
-      alert('Password minimal 6 karakter, hanya huruf, angka, atau simbol !@#$%^&*');
+    if (getDataPengguna.password!=getInputPassword.value.trim()) {
+      alert('Password Salah');
       return;
     }
     document.location.href = 'dashboard.html'; // ganti sesuai halaman tujuan
